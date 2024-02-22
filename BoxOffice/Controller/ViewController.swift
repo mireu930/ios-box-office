dca969169056d780af5c27e77fdc3d23d28daf9d
mireu <148876644+mireu930@users.noreply.github.com>
2024-02-22 23:32:12 +0900

    feat: collectionView 오토레이아웃구현

--- BoxOffice/Controller/ViewController.swift
+++ BoxOffice/Controller/ViewController.swift
@@ -9,20 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     private var collectionView: UICollectionView = {
-           let layout = UICollectionViewFlowLayout()
-           layout.scrollDirection = .vertical
-
-           let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-           collectionView.backgroundColor = .systemYellow
-           collectionView.translatesAutoresizingMaskIntoConstraints = false
-
-           return collectionView
-       }()
-
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemYellow
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        autoLayout()
     }
-
+    
+    private func autoLayout() {
+        view.addSubview(collectionView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
